a08d28622f273eb5c8e457e1345c0e0644f0a0d3
evertondewes <everton.dewes@gmail.com>
2018-08-22 20:23:52 -0300

    Signed-off-by: evertondewes <everton.dewes@gmail.com>

---
+++ BancoDados_20180822_01.sql
@@ -0,0 +1,11 @@
+SET SQL_SAFE_UPDATES = 0;
+
+insert into pessoa(nome, sobrenome) values("Everton", "Dewes");
+insert into pessoa(nome, sobrenome) values("Albert", "Einstein");
+insert into pessoa(nome, sobrenome) values("Isaac", "Newton");
+
+select * from pessoa;
+
+delete from pessoa;
+
+
